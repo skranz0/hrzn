0543586f3e7107528c272186488d4db3f1dae7d7
Stefan Kranz <kranz.stefan99@gmail.com>
2025-04-14 15:10:09 +0200

    skeleton

---
+++ src/functions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# create exchange link file
+function create_xlnk () {
+    echo "Creating exchange link file..."
+}
+
+function hrzn_push () {
+    echo "Pushing file to exchange..."
+}
+
+function hrzn_pull () {
+    echo "Pulling file from exchange..."
+}
+
+function hrzn_check () {
+    echo "Checking file integrity..."
+}
---
+++ src/hrzn.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# import funtions
+source functions.sh
+
+# function for help text
+function show_help() {
+    echo "Usage: hrzn.sh [options] <file>"
+    echo "Options:"
+    echo "  -h, --help        Show this help message"
+    echo "  push        Push file to exchange"
+    echo "  pull        Pull file from exchange"
+    echo "  check       Check file integrity"
+    echo "  link        Create exchange link file"
+}
+
+# main logic
+if [[ $# -eq 0 ]]; then
+    show_help
+    exit 1
+fi
+
+# first argument is the subcommand
+command="$1"
+shift # remove first parameter
+
+case "$command" in
+    -h|--help)
+        show_help
+        ;;
+    push)
+        hrzn_push "$@"
+        ;;
+    pull)
+        hrzn_pull "$@"
+        ;;
+    check)
+        hrzn_check "$@"
+        ;;
+    link)
+        create_xlnk "$@"
+        ;;
+    *)
+        echo "Unknown command: $command"
+        show_help
+        exit 1
+        ;;
+esac
