75e0e1fcc63ef9c9adc5c6343b90c91aad875990
Stefan Kranz <kranz.stefan99@gmail.com>
2025-04-23 14:33:23 +0200

    accept multiple files

--- lib/pull.sh
+++ lib/pull.sh
@@ -12,8 +12,8 @@ NC='\033[0m' # No Color
 
 function hrzn_pull () {
     function show_help () {
-        echo "Pull file from external storage."
-        echo "Usage: hrzn.sh [options] <verge-file>"
+        echo "Pull files from external storage."
+        echo "Usage: hrzn.sh [options] <verge-file> ..."
         echo "Options:"
         echo "  -h, --help        Show this help message"
     }
@@ -22,40 +22,42 @@ function hrzn_pull () {
         exit 1
     fi
 
-    verge_file="$1"
-    if [[ ! -f "$verge_file" ]]; then
-        echo "$RED Linkage file not found:$NC $verge_file"
-        exit 1
-    fi
-
-    while IFS= read -r line; do
-        echo "$line"
-        if [[ $line == path_x* ]]; then
-            path_x=$(echo "$line" | cut -d' ' -f5)
-            echo "$YELLOW Retrieving path from linkage file:$NC $path_x"
-        elif [[ $line == checksum_x* ]]; then
-            checksum_x=$(echo "$line" | cut -d' ' -f5)
+    for verge_file in "@"
+    do
+        if [[ ! -f "$verge_file" ]]; then
+            echo "$RED Linkage file not found:$NC $verge_file"
+            exit 1
         fi
-    done < "$verge_file"
-    if [[ ! -f "$path_x" ]]; then
-        echo "$RED File not found in external storage:$NC $path_x"
-        exit 1
-    fi
-    echo "$YELLOW Copying file from external storage...$NC"
-    cp -i "$path_x" .
-    checksum_local=$(md5sum "$path_x" | cut -d' ' -f1)
-    echo "$YELLOW Comparing checksums...$NC"
-    echo "Checksum local: $checksum_local"
-    echo "Checksum external: $checksum_x"
-    if [[ "$checksum_local" == "$checksum_x" ]]; then
-        echo "$GREEN File integrity check passed.$NC"
-    else
-        echo "$RED File integrity check failed.$NC"
-        exit 1
-    fi
-    echo "$GREEN File pulled from external storage:$NC $path_x"
-    rm "$path_x"
-    echo "$GREEN File removed from external storage:$NC $path_x"
-    rm "$verge_file"
-    echo "$GREEN Linkage file removed:$NC $verge_file"
+
+        while IFS= read -r line; do
+            echo "$line"
+            if [[ $line == path_x* ]]; then
+                path_x=$(echo "$line" | cut -d' ' -f5)
+                echo "$YELLOW Retrieving path from linkage file:$NC $path_x"
+            elif [[ $line == checksum_x* ]]; then
+                checksum_x=$(echo "$line" | cut -d' ' -f5)
+            fi
+        done < "$verge_file"
+        if [[ ! -f "$path_x" ]]; then
+            echo "$RED File not found in external storage:$NC $path_x"
+            exit 1
+        fi
+        echo "$YELLOW Copying file from external storage...$NC"
+        cp -i "$path_x" .
+        checksum_local=$(md5sum "$path_x" | cut -d' ' -f1)
+        echo "$YELLOW Comparing checksums...$NC"
+        echo "Checksum local: $checksum_local"
+        echo "Checksum external: $checksum_x"
+        if [[ "$checksum_local" == "$checksum_x" ]]; then
+            echo "$GREEN File integrity check passed.$NC"
+        else
+            echo "$RED File integrity check failed.$NC"
+            exit 1
+        fi
+        echo "$GREEN File pulled from external storage:$NC $path_x"
+        rm "$path_x"
+        echo "$GREEN File removed from external storage:$NC $path_x"
+        rm "$verge_file"
+        echo "$GREEN Linkage file removed:$NC $verge_file"
+    done
 }
--- lib/push.sh
+++ lib/push.sh
@@ -17,8 +17,8 @@ source "/etc/hrzn/config"
 # function definitions
 function hrzn_push () {
     function show_help () {
-        echo "Push file to external storage and create linkage file."
-        echo "Usage: hrzn.sh [options] <file>"
+        echo "Push files to external storage and create linkage file."
+        echo "Usage: hrzn.sh [options] <file> ..."
         echo "Options:"
         echo "  -h, --help        Show this help message"
 
@@ -28,41 +28,43 @@ function hrzn_push () {
         exit 1
     fi
 
-    file="$1"
-    # check if file exists
-    if [[ ! -f "$file" ]]; then
-        echo "$RED File not found: $file $NC"
-        exit 1
-    fi
+    for file in "$@"
+    do
+        # check if file exists
+        if [[ ! -f "$file" ]]; then
+            echo "$RED File not found: $file $NC"
+            exit 1
+        fi
 
-    # copy file to external storage
-    echo "$YELLOW Copying file to external storage...$NC"
-    cp -i "$file" "$external_storage"
-    # calculate and compare checksums
-    echo "$YELLOW Calculating checksums...$NC"
-    checksum_origin=$(md5sum "$file" | cut -d' ' -f1)
-    checksum_external=$(md5sum "$external_storage""$file" | cut -d' ' -f1)
-    echo "Checksum origin: $checksum_origin"
-    echo "Checksum external: $checksum_external"
-    if [[ "$checksum_origin" == "$checksum_external" ]]; then
-        echo "$GREEN File integrity check passed.$NC"
-    else
-        echo "$RED File integrity check failed.$NC"
-        exit 1
-    fi
+        # copy file to external storage
+        echo "$YELLOW Copying file to external storage...$NC"
+        cp -i "$file" "$external_storage"
+        # calculate and compare checksums
+        echo "$YELLOW Calculating checksums...$NC"
+        checksum_origin=$(md5sum "$file" | cut -d' ' -f1)
+        checksum_external=$(md5sum "$external_storage""$file" | cut -d' ' -f1)
+        echo "Checksum origin: $checksum_origin"
+        echo "Checksum external: $checksum_external"
+        if [[ "$checksum_origin" == "$checksum_external" ]]; then
+            echo "$GREEN File integrity check passed.$NC"
+        else
+            echo "$RED File integrity check failed.$NC"
+            exit 1
+        fi
 
-    # create link file
-    echo "$YELLOW Creating linkage file...$NC"
-    touch "$file.verge"
-    {
-        echo "path_o    $(pwd)/$file";
-        echo "path_x    $external_storage$file";
-        echo "checksum_o    $checksum_origin";
-        echo "checksum_x    $checksum_external";
-    } >> "$file.verge"
-    echo "$GREEN Linkage file created:$NC $file.verge"
-    echo "$GREEN File pushed to external storage:$NC '$external_storage$file'"
+        # create link file
+        echo "$YELLOW Creating linkage file...$NC"
+        touch "$file.verge"
+        {
+            echo "path_o    $(pwd)/$file";
+            echo "path_x    $external_storage$file";
+            echo "checksum_o    $checksum_origin";
+            echo "checksum_x    $checksum_external";
+        } >> "$file.verge"
+        echo "$GREEN Linkage file created:$NC $file.verge"
+        echo "$GREEN File pushed to external storage:$NC '$external_storage$file'"
 
-    rm "$file"
-    echo "$GREEN Original file removed$NC"
+        rm "$file"
+        echo "$GREEN Original file removed$NC"
+    done
 }
