061298f030c3a512ab5b7c0b9327bc811f536741
Stefan Kranz <stefan.kranz@landw.uni-halle.de>
2025-09-23 10:49:38 +0200

    add uninstallation function

--- hrzn.sh
+++ hrzn.sh
@@ -7,6 +7,7 @@
 #############################
 
 # import functions
+
 source /usr/local/bin/lib/hrzn/push.sh
 source /usr/local/bin/lib/hrzn/pull.sh
 source /usr/local/bin/lib/hrzn/move.sh
@@ -14,6 +15,7 @@ source /usr/local/bin/lib/hrzn/show_x.sh
 source /usr/local/bin/lib/hrzn/change_external.sh
 
 # function for help text
+
 function show_help() {
     echo "Usage: hrzn.sh [options] <file>"
     echo "Options:"
@@ -23,6 +25,7 @@ function show_help() {
     echo "  move            Change the origin path in a verge file"
     echo "  show_x          Show the path of the external storage"
     echo "  change-external Find verge files and change the path of the external storage"
+    echo "  uninstall       Uninstall hrzn from the system"
 }
 
 ## main logic ##
@@ -33,8 +36,44 @@ if [[ $# -eq 0 ]]; then
 fi
 
 # first argument is the subcommand
+
 command="$1"
 shift # remove first parameter
+
+function uninstall () {
+    echo "This will remove hrzn and all its resources from the system."
+    echo "Continue?"
+    select continue in "Yes" "No"; do
+        case $continue in
+            Yes ) break;;
+            No ) exit;;
+        esac
+    done
+    echo "Search for remaining verge files before?"
+    select search in "Yes" "No"; do
+        case $search in
+            Yes ) {
+                read -p "Enter starting directory to start recursive search: " search_start
+                remaining_verges=$(find $search_start -type f -name "*verge")
+                echo "${#remaining_verges[@]} verge files found. Continue with deinstallation anyway?"
+                select after_search in "Yes" "No"; do
+                    case $after_search in
+                        Yes ) break;;
+                        No ) exit;;
+                    esac
+                done
+            };;
+            No ) break;;
+        esac
+    done
+    echo "Removing auxillary scripts"
+    rm -r /usr/local/bin/lib/hrzn
+    echo "Removing config file"
+    rm -r /etc/hrzn
+    echo "Removing main file"
+    rm /usr/local/bin/hrzn 
+    echo "Uninstallation finished"
+}
 
 case "$command" in
     -h|--help)
@@ -54,6 +93,9 @@ case "$command" in
         ;;
     change-external)
         change-external
+        ;;
+    uninstall)
+        uninstall
         ;;
     *)
         echo "Unknown command: $command"
