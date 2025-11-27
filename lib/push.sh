2d04013d6aca16e317b6b95874bf63927dc620f3
Stefan Kranz <stefan.kranz@landw.uni-halle.de>
2025-11-27 15:04:07 +0100

    add horizon parameter

--- lib/push.sh
+++ lib/push.sh
@@ -10,23 +10,32 @@ RED='\033[0;31m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
-# import config with path to external storage
-external_storage=""
-source "/etc/hrzn/config"
+HORIZONS="ASM|ALN|VAR|COR"
+
+external_storage="/nucleus/results"
 
 # function definitions
 function hrzn_push () {
     function show_help () {
         echo "Push files to external storage and create linkage file."
-        echo "Usage: hrzn push [options] <file> ..."
+        echo "Usage: hrzn push [options] <horizon> <file> ..."
+        echo ""
+        echo "<horizon>		Where to put the file. Options are $HORIZONS"
+        echo "<file>		File to store away. Supports wildcards"
         echo "Options:"
         echo "  -h, --help        Show this help message"
-
     }
     if [[ $# -eq 0 || "$1" == "-h" || "$1" == "--help" ]]; then
         show_help
         exit 1
     fi
+
+    local horizon=$1
+    if ! [[ $horizon =~ ^(ALN|ASM|COR|VAR)$ ]]; then
+        echo "Unknown horizon. Options are $HORIZONS"
+        exit 1
+    fi
+    shift
 
     # go through each file given as CLI parameter
     for file in "$@"
@@ -39,11 +48,11 @@ function hrzn_push () {
 
         # copy file to external storage
         echo -e "${YELLOW}Copying file to external storage...${NC}"
-        cp -i "$file" "$external_storage"
+        cp -i "$file" "$external_storage/$horizon"
         # calculate and compare checksums
         echo -e "${YELLOW}Calculating checksums...${NC}"
         checksum_origin=$(md5sum "$file" | cut -d' ' -f1)
-        checksum_external=$(md5sum "$external_storage""$file" | cut -d' ' -f1)
+        checksum_external=$(md5sum "$external_storage/$horizon/$file" | cut -d' ' -f1)
         echo "Checksum origin: $checksum_origin"
         echo "Checksum external: $checksum_external"
         if [[ "$checksum_origin" == "$checksum_external" ]]; then
@@ -53,22 +62,22 @@ function hrzn_push () {
             exit 1
         fi
 
-        # create link file
-        echo -e "${YELLOW}Creating linkage file...${NC}"
+        # create verge file
+        echo -e "${YELLOW}Creating verge file...${NC}"
         touch "$file.verge"
         {
             echo "path_o    $(pwd)/$file";
-            echo "path_x    $external_storage$file";
+            echo "path_x    $external_storage/$horizon/$file";
             echo "checksum_o    $checksum_origin";
             echo "checksum_x    $checksum_external";
             echo "date_pushed $(date)"
         } >> "$file.verge"
-        
+
         # copy verge file to external storage as well
         cp "$file.verge" "$external_storage$file.verge"
-        echo -e "${GREEN}Linkage file created:${NC} $file.verge"
-        echo -e "${GREEN}File pushed to external storage:${NC} '$external_storage$file'"
-        
+        echo -e "${GREEN}Verge file created:${NC} $file.verge"
+        echo -e "${GREEN}File pushed to horizon:${NC} $external_storage/$horizon/$file"
+
         # remove original file
         rm "$file"
         echo -e "${GREEN}Original file removed${NC}"
